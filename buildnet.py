f04a1682be7fb93ff05d81af6774262eb8c2a81f
GuyLeib <90501635+GuyLeib@users.noreply.github.com>
2023-06-11 12:46:54 +0300

    Upload py file

---
+++ buildnet.py
@@ -0,0 +1,8 @@
+# Init a  population of neural network - aka weights.
+# The nn is 2-3 layers - random between 2-4
+# The size of each layer also a random parameter - 5-10
+# Activation function - sigmoid
+# Train the nn
+# fitness = evaluation the nn using the loss function
+# Use GA to make changes in the weights - instead of back propagation - mutation, crossover, elitism.
+# Choose the model weights and structure using the test set.
